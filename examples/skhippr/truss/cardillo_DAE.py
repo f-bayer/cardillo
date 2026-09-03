0dac2bb48cbd13dca263928b437bb5fe49f2e19e
Marco Herrmann <m.herrmann@tue.nl>
2026-09-03 12:15:39 +0200

    truss: vtk_export and show_plots optional; damper's anchor further to the right; state for paraview

--- examples/skhippr/truss/cardillo_DAE.py
+++ examples/skhippr/truss/cardillo_DAE.py
@@ -27,7 +27,14 @@ from cardillo.solver.skhippr import (
 )
 
 
-def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
+def main(
+    static_continuation=True,
+    hbm_single=True,
+    hbm_continuation=True,
+    compliance_form=True,
+    export_vtk=False,
+    show_plots=True,
+):
     # parameters
     a = 1.0
     mass = 1.0
@@ -46,12 +53,13 @@ def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
     inter_spring = TwoPointInteraction(
         cardillo_system.origin, rb, B_r_CP1=np.array([0, 0, a])
     )
-    spring = Spring(inter_spring, k=k_spring, l_ref=l0, compliance_form=False)
+    spring = Spring(inter_spring, k=k_spring, l_ref=l0, compliance_form=compliance_form)
 
     # damper
     inter_damper = TwoPointInteraction(
-        cardillo_system.origin, rb, B_r_CP1=np.array([a, 0, 0])
+        cardillo_system.origin, rb, B_r_CP1=np.array([2 * a, 0, 0])
     )
+    # damping force always with compliance_form=False, as k is 0!
     damper = KelvinVoigtElement(inter_damper, k=0.0, d=d_damper, compliance_form=False)
 
     # force (here [0, 0, 0], later updated to what is required for static_continuation, hbm_single or hbm_continuation)
@@ -69,13 +77,15 @@ def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
         cardillo_solver = SkhipprStaticContinuation(cardillo_system)
         sol, skhippr_sol = cardillo_solver.solve()
 
-        # vtk-export
-        dir_name = Path(__file__).parent
-        cardillo_system.export(dir_name, "vtk_static_continuation", sol)
+        if export_vtk:
+            # vtk-export
+            dir_name = Path(__file__).parent
+            cardillo_system.export(dir_name, "vtk_static_continuation", sol)
 
         # SKHiPPr plot continuation
         plot_continuation(skhippr_sol)
-        plt.show()
+        if show_plots:
+            plt.show()
 
     if hbm_single:
         # specify force
@@ -83,12 +93,13 @@ def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
         force.force = lambda t: np.array([0.5 * np.sin(omega * t) - 0.5, 0, 0])
 
         # solver
-        cardillo_solver = SkhipprHBM(cardillo_system, omega)
+        cardillo_solver = SkhipprHBM(cardillo_system, omega, newton_max_iter=40)
         sol, skhippr_sol = cardillo_solver.solve()
 
-        # vtk-export
-        dir_name = Path(__file__).parent
-        cardillo_system.export(dir_name, "vtk_hbm", sol)
+        if export_vtk:
+            # vtk-export
+            dir_name = Path(__file__).parent
+            cardillo_system.export(dir_name, "vtk_hbm", sol)
 
         # SKHiPPR plots
         plot_period(skhippr_sol, title=f"initial periodic solution")
@@ -97,11 +108,13 @@ def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
             idx=[0, cardillo_system.nq],
             title=f"initial periodic solution",
         )
-        plt.show()
+        if show_plots:
+            plt.show()
 
     if hbm_continuation:
         # specify force via update function for skhippr continuation
         omega = 0.01
+
         def set_parameter(F):
             force.force = lambda t, F=F: np.array([F * np.sin(omega * t) - 0.5, 0, 0])
 
@@ -117,12 +130,15 @@ def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
         )
         solutions, branch = solver.solve()
 
-        # vtk-export
-        dir_name = Path(__file__).parent
-        solutions_export = solutions[:: int(np.max([1, len(solutions) / 10]))]
-        for i, sol in enumerate(solutions_export):
-            # same parameter value can exist in multiple solutions (continuation)!
-            cardillo_system.export(dir_name, f"vtk_hbm_{i}__param_{sol.param:.4f}", sol)
+        if export_vtk:
+            # vtk-export
+            dir_name = Path(__file__).parent
+            solutions_export = solutions[:: int(np.max([1, len(solutions) / 10]))]
+            for i, sol in enumerate(solutions_export):
+                # same parameter value can exist in multiple solutions (continuation)!
+                cardillo_system.export(
+                    dir_name, f"vtk_hbm_{i}__param_{sol.param:.4f}", sol
+                )
 
         # SKHiPPR plots
         def plot_fun(bp):
@@ -136,11 +152,12 @@ def main(static_continuation=True, hbm_single=True, hbm_continuation=True):
             xlabel="parameter (F)",
             ylabel="amplitude of q[0]",
         )
-
         _ = animate_phase(branch, idx=[0, cardillo_system.nq], interval=60)
-
-        plt.show()
+        if show_plots:
+            plt.show()
 
 
 if __name__ == "__main__":
-    main()
+    main(False, True, False, compliance_form=True, show_plots=False)
+    main(False, True, False, compliance_form=False, show_plots=False)
+    plt.show()
